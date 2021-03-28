19210cc5e0a2e8e03656d3a2a6cc452a5a0a5bca
cth <xxkehuxx@gmail.com>
2021-03-28 14:22:39 +0300

    ???

--- cars.py
+++ cars.py
@@ -1,8 +1,11 @@
 import os
 import pandas as pd
+import numpy as np
 import missingno as msno
 import matplotlib.pyplot as plt
 import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
 
 pd.set_option('display.max_rows', 50)
 pd.set_option('display.min_rows', 20)
@@ -52,3 +55,55 @@ df[numeric] = df[numeric].astype('float')
 df[categorical] = df[categorical].astype('category')
 cor_mat = df.corr()
 sns.heatmap(cor_mat, vmin=-1, vmax = 1, linecolor='black', linewidths=.1, center = 1)
+
+sns.histplot(df['Price'], color = 'black')
+
+
+##################
+# Train/test split
+num_training = int(0.8 * df['Price'].count())
+num_test = df['Price'].count() - num_training
+
+# Training data
+X_train = np.array(df['Mileage'][:num_training]).reshape((num_training,1))
+y_train = np.array(df['Price'][:num_training])
+
+# Test data
+X_test = np.array(df['Mileage'][num_training:]).reshape((num_test,1))
+y_test = np.array(df['Price'][num_training:])
+
+# Create linear regression object
+from sklearn import linear_model
+
+linear_regressor = linear_model.LinearRegression()
+
+# Train the model using the training sets
+linear_regressor.fit(X_train, y_train)
+y_test_pred = linear_regressor.predict(X_test)
+
+plt.scatter(X_test, y_test, color='green', marker = '.', linewidths=1)
+plt.plot(X_test, y_test_pred, color='black', linewidth=1)
+plt.xticks(())
+plt.yticks(())
+plt.show()
+
+
+import sklearn.metrics as sm
+
+print("Mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred), 2))
+print("Mean squared error =", round(sm.mean_squared_error(y_test, y_test_pred), 2))
+print("Median absolute error =", round(sm.median_absolute_error(y_test, y_test_pred), 2))
+print("Explain variance score =", round(sm.explained_variance_score(y_test, y_test_pred), 2))
+print("R2 score =", round(sm.r2_score(y_test, y_test_pred), 2))
+
+output_model_file = '3_model_linear_regr.pkl'
+
+with open(output_model_file, 'wb') as f:
+    pickle.dump(linear_regressor, f)
+
+with open(output_model_file, 'rb') as f:
+    model_linregr = pickle.load(f)
+
+y_test_pred_new = model_linregr.predict(X_test)
+print("\nNew mean absolute error =", round(sm.mean_absolute_error(y_test, y_test_pred_new), 2))
+
