788efad76f2b56a0c73c6a83633402b73020cf3b
cth <xxkehuxx@gmail.com>
2021-05-26 08:31:05 +0300

    1

--- depress.py
+++ depress.py
@@ -14,9 +14,9 @@ pd.set_option('expand_frame_repr', True)
 #  Read all csv files. Control group / Condition group / Total scores
 
 
-path1 = 'C://Users/cth/PycharmProjects/sources/data/scores.csv'
-path2 = 'C://Users/cth/PycharmProjects/sources/data/condition/'
-path3 = 'C://Users/cth/PycharmProjects/sources/data/control/'
+path1 = 'C:/Users/cth/PycharmProjects/_to_do/depres/scores.csv'
+path2 = 'C:/Users/cth/PycharmProjects/_to_do/depres/condition/'
+path3 = 'C:/Users/cth/PycharmProjects/_to_do/depres/control/'
 cond_df = pd.DataFrame()
 control_df = pd.DataFrame()
 scores  = pd.read_csv(path1)
@@ -43,11 +43,23 @@ for i in os.listdir(path3):
 #######################################################################################
 
 scores['madrs_delta'] = scores.madrs2 - scores.madrs1
+categories = ['gender' ,'afftype', 'melanch', 'inpatient', 'marriage', 'work', 'edu']
+scores[categories] = scores[categories].astype('category')
+scores['group'] = np.where(scores.number.str.contains('condition'),'condition','control' )
+
+cond_df['timestamp'] = pd.to_datetime(cond_df['timestamp'], format = '%Y-%m-%d %H:%M:%S')
+cond_df.set_index('timestamp', inplace = True)
+grouped = cond_df.groupby(['file'])
+
+for x,y in grouped:
+    for i in range(1,9):
+        plt.subplot(3,3, i)
+        plt.plot(y['date'], y['activity'])
 
 
-grouped = cond_df.groupby(['file', 'date'])
-cond_list = list(zip(*cond_df.groupby('file')))
-cond_list = cond_list[1]
-for index, i in enumerate(cond_list):
-    print(index)
-    print(i['activity'].mean())
+# grouped = cond_df.groupby(['file', 'timestamp'])['activity']
+# plt.bar(pers_activ.index, pers_activ.values)
+# pers_activ.droplevel('file')
+# for index, (i,j), z in pers_activ:
+#     plt.subplot(3,3,index + 1)
+#     plt.bar(i, z)
--- primefactorial.py
+++ primefactorial.py
@@ -1,55 +1,25 @@
-"2^19 * 3^9 * 5^4 * 7^3 * 11^2 * 13 * 17 * 19"
-import itertools
-from functools import reduce
-import math
-import collections
-from operator import mul
-
-# Function to determine whether the number is prime
+import functools
 
 def prime(n):
-    if n <= 3:
-        return True
-    else:
-        for i in range(2, int(n**0.5 + 1)):
+    if n > 1:
+        if n <= 3:
+            return True
+        for i in range(2,int(n ** 0.5 + 1)):
             if n % i == 0:
                 return False
         return True
 
+def find_even(n):
+    num = 0
+    for i in str(n):
+        if int(i) in [2,4,6,8,0]:
+            num = num + 1
+        pass
+    return num
 
-# Function for decomposition "n"
-def decomp(n):
-    # n! value calculating
-    n_fact = math.factorial(n)
-    # all the parts of factorial sequence
-    n_parts = [i for i in range(n, 2, -1)]
-    ans = []
+@functools.lru_cache(1000)
+def f(n):
+    ans = [(find_even(i), i) for i in range(n) if prime(i)]
+    return sorted(ans)[-1][1]
 
-# If number is prime -> power = 1
-    for i in n_parts:
-        if prime(i):
-            ans.append(i)
-            n_parts.remove(i)
-
-
-# Production of all numbers in the updated list
-    new_num = reduce(mul, n_parts, 1)
-# Divide number by 2 till it's divisible by 2. Otherwise a = 3, divide by 3... etc.
-    a = 2
-    while not prime(new_num):
-        if new_num % a == 0:
-            new_num = new_num / a
-            ans.append(a)
-        else:
-            a += 1
-
-
-    for i in ans:
-        if ans.count(i) == 1:
-
-# Counting the number of divisions of each number
-    counts = collections.Counter(ans)
-
-    return ' * '.join([(str(i)+'^'+str(counts[i])) for i in sorted(counts)])
-
-
+f(10000)
